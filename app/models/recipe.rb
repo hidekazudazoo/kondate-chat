c69058f6dfeb4da1aba2bfe7d8b62b1120de1b60
hidekazudazoo <118248290+hidekazudazoo@users.noreply.github.com>
2023-03-04 19:03:56 +0900

    バリデーションの設定

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,13 @@
 class Recipe < ApplicationRecord
+  belongs_to :category
+  belongs_to :user
+  has_one_attached :image
+  with_options presence: true do
+    validates :title
+    validates :category_id, numericality: { other_than: 0, message: "選択してください" }
+    validates :headline
+    validates :ingredient
+    validates :derection
+    validates :image
+  end
 end
