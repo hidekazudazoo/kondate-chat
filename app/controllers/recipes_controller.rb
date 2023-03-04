cf4a49e880c34e1a45611a19807f91e9d04782c7
hidekazudazoo <118248290+hidekazudazoo@users.noreply.github.com>
2023-03-04 18:49:31 +0900

    ルーティングの設定、コントローラーの作成

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,18 @@
 class RecipesController < ApplicationController
+
   def index
+    @recipes = Recipe.all
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    Recipe.create(recipe_params)
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :category_id, :headline, :ingredient, :derection, :memo, :image).merge(user.id)
 end
