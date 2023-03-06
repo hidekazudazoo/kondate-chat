007cd538e0042316571d671c1a4eeebdf9b7b0d5
hidekazudazoo <118248290+hidekazudazoo@users.noreply.github.com>
2023-03-07 06:41:42 +0900

    pry-railsの導入

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,7 +9,12 @@ class RecipesController < ApplicationController
   end
 
   def create
-    Recipe.create(recipe_params)
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
