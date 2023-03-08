53f191ee5e5c8452ad4029a09281ad0fcbe314dd
hidekazudazoo <118248290+hidekazudazoo@users.noreply.github.com>
2023-03-09 06:58:01 +0900

    テストコードの実行

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :recipe do
-    
+    title {"タイトル"}
+    category_id {1}
+    headline {"メインコピーです"}
+    ingredient {"材料です"}
+    direction {"作り方です"}
+    memo {"メモです"}
+    association :user
+    after(:build) do |recipe|
+      recipe.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
