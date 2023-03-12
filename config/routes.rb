8da19ddf7a94534308919f32f52e64beb9ec7253
hidekazudazoo <118248290+hidekazudazoo@users.noreply.github.com>
2023-03-12 09:55:37 +0900

    コメントコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "recipes#index"
   resources :users, only: :show
-  resources :recipes, only:[:index, :new, :create, :show]
+  resources :recipes, only:[:index, :new, :create, :show] do
+    resources :comments, only: :create
+  end
 end
