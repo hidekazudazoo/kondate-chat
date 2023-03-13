1f6eacaeed48d50dc08f44de126272fe4a0955d9
hidekazudazoo <118248290+hidekazudazoo@users.noreply.github.com>
2023-03-13 21:43:43 +0900

    お気に入り非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,19 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
+  before_action :recipe_find
 
   def create
     Favorite.create(user_id: current_user.id, recipe_id: params[:id])
-    redirect_to root_path
   end
 
   def destroy
     favorite = Favorite.find_by(user_id: current_user.id, recipe_id: params[:id])
     favorite.destroy
-    redirect_to root_path
   end
+
+  private
+  def recipe_find
+    @recipe = Recipe.find(params[:id])
+  end
+
 end
