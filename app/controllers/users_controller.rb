77a30b522571f95c9cb322e5c6c210ea213a7dc2
hidekazudazoo <118248290+hidekazudazoo@users.noreply.github.com>
2023-03-18 15:39:37 +0900

    自分自身のマイページへのアクセス制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @user = User.find(params[:id])
     @nickname = current_user.nickname
+    if @user.id != current_user.id
+      redirect_to root_path
+    end
   end
+
 end
